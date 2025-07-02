0d6d97edf049adc28e6b8b857c5617959bc416e1
Sahil Singh <143764954+sahxzm@users.noreply.github.com>
2025-07-03 05:18:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  serverComponentsExternalPackages: ['fs', 'path', 'os', 'child_process'],
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-      os: false,
-      child_process: false,
-    };
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
